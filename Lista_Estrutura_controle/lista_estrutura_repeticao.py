1b6287d863a52645d8dcb2ce86e3c9c617a4840b
HanSolon <52141000+HanSolon@users.noreply.github.com>
2021-06-17 15:20:32 -0400

    commit refomulando qts 1-10

--- Lista_Estrutura_controle/lista_estrutura_repeticao.py
+++ Lista_Estrutura_controle/lista_estrutura_repeticao.py
@@ -1,62 +1,73 @@
 """ 1 - Faça um programa que determine e mostre os cincos 
-primeiros múltiplos de 3, considerando números maiores que 0.
-for n in range(1, 6):
-    print(n * 3)"""
+primeiros múltiplos de 3, considerando números maiores que 0."""
+"""for n in range(1, 16):
+    if n % 3 == 0:
+        print(n)"""
 
 """2 - Escreva um programa que escreva na tela, de 1 até 100, de 1 
 em 1, 3 vezes. A primira vez deve usar a estrutura de repetição
-for, a segunda while, e a terceira do while. 
-
+for, a segunda while, e a terceira do while.""" 
+"""
 for n in range(1, 101):
     print(n)
 print("\nPROXIMA SEQUENCIA\n")
-minimo = 1
+int: minimo = 1
 while minimo < 101:
     print(minimo)
     minimo += 1
 print("\nPROXIMA SEQUENCIA\n")
-numero = 0
+int: numero = 0
 while True:
-    numero += 1
-    print(numero)
-    if numero == 100:
-        break"""
+    if numero < 100:
+        numero += 1
+        print(numero)
+    else:
+        break
+"""
 
 """ 3 - Faça um algoritimo utilizando o comando while que mostra
 uma constagem regressiva na tela, iniciando em 10 e terminando em 0.
-Mostrar um mensagem 'FIM!', após a contagem
-regressiva = 11
+Mostrar um mensagem 'FIM!', após a contagem"""
+"""regressiva: int = 11
 while regressiva > 0:
     regressiva -= 1
     print(regressiva)
 print('FIM!')"""
 
-""" 4 - Escreva um programa que declare um inteir, inicialize-o
+""" 4 - Escreva um programa que declare um inteiro, inicialize-o
 com 0, e incremente-o de 1000 em 1000, imprimindo seu valor na
-tela, até que seu valor seja 100000(cem mil).
-for n in range(1000, 100001, 1000):
-    print(n)"""
-
+tela, até que seu valor seja 100000(cem mil)."""
+"""for n in range(0, 100001, 1000):
+    print(n)
+"""
 """ 5 - Faça um programa que peça ao usuario para digitar
-10 valores e some-os
-soma = 0 
+10 valores e some-os"""
+"""soma: int = 0 
 for valor in range(1, 11):
-    valor = int(input("Digite um valor inteiro: "))
+    valor: int = int(input("Digite um valor inteiro: "))
     soma += valor
 print(soma)"""
 
-""" 6 - Faça um programa que leia 10 inteiros e imprima sua meédia.
-soma = 0
+""" 6 - Faça um programa que leia 10 inteiros e imprima sua média."""
+"""soma: int = 0
 for i in range(10):
-    valor = int(input("Informe o valor: "))
+    valor: int = int(input("Informe o valor: "))
     soma += valor
 print("A média é", soma / 10)"""
 
 """ 7 - Faça um programa que leia 10 inteiros positivos, ignorando 
-não positivos, e imprima sua media. 
-
-soma = 0
-for qtd in range(4):
+não positivos, e imprima sua media.""" 
+"""qtd: int = 0
+soma: int = 0
+while qtd != 10:  
+  qtd += 1
+  numero: int = int(input("Digite o seu número: "))
+  while numero <= 0:
+       numero: int = int(input("O número digitado deve ser positivo"))
+  soma += numero
+print(f"A média é {soma/10}")"""
+    
+"""for qtd in range(4):
     valor = int(input(" Informe o valor: "))
     if valor >= 0:
         soma += valor
@@ -65,7 +76,20 @@ for qtd in range(4):
 print("A media é: ", soma / 4)"""
 
 """ 8 - Escreva um programa que leia 10 numeros eescreva o menor
-valor e o maior valor lido. 
+valor e o maior valor lido. """
+"""num: int = int(input("Digite valor: ")) 
+maior: int = num
+menor: int = num
+qtd: int = 1
+while qtd != 10:
+    qtd += 1
+    num: int = int(input("Digite numero: "))
+    if num > maior:
+        maior = num
+    elif num < menor:
+        menor = num
+print(maior, menor)
+
 lista = []
 for numeros in range(1, 11):
     valor = int(input("Informe um numero: "))
@@ -75,29 +99,20 @@ print("Maior valor: ", max(lista))
 print("Menor valor: ", min(lista))"""
 
 """ 9 - Faça um programa que leia um número inteiro N e depois imprima
-os N primeiros numeros naturais ímpares
-valor = int(input("Informe um numero N: "))
-impares = []
+os N primeiros numeros naturais ímpares"""
+"""valor: int = int(input("Informe um numero N: "))
 while valor > 0:
     valor -= 1
     if valor % 2 != 0:
-        impares.append(valor)        
-print("Impares: ", impares)"""
+        print(valor)      
+"""
 """ 10 - Faça um programa que calcule e mostre a soma dos 50 primeiros 
-números pares 
-numero = int(input("Informe um numero > 50: "))
-soma = 0
-lista = []
-if numero > 50:
-    while numero > 0:
-        numero -= 1
-        if numero % 2 == 0:
-            lista.append(numero) 
-else: 
-    print("Numero < 50") 
-print("A soma dos pares: ", sum(lista))
-print(lista)"""
-
+números pares""" 
+"""soma: int = 0
+for num in range(51):
+    if num % 2 == 0:
+        soma += num
+print(soma)"""
 """11 - Faça um programa que leia um numero inteiro N e imprima todos os 
 números naturais de 0 até N em ordem crescente.
 numero = int(input("Digite um numero N: "))
@@ -263,13 +278,13 @@ print(sum(lista))
 print(lista)"""
 """26 - Faça um algoritimo que encontre o primeiro multiplo
 11, 13 ou 17 após um numero de dados""" 
-numero: int = int(input("Digite um numero: "))
+"""numero: int = int(input("Digite um numero: "))
 lista = []
 for num in range(numero, numero + 12):
     if num % 11 == 0 or num % 13 == 0 or num % 17 == 0:
         lista.append(num)
 print(lista)
-
+"""
 """27 - Em Matemática, o numero harmonico designado por H(n) 
 define-se como sendo a soma da série harmónica: 
 H(n) = 1 + 1/2 + 1/3 + 1/4 +...+ 1/n
